c912f6a35eb3041c4857eb6a381ab8dc8cfd18b2
DenysChuma <Chumiks1999@gmail.com>
2023-11-06 17:55:13 +0200

    add message reply, edit. Content view finished. Work on styles.

---
+++ src/app/layout/master-chat/master-chat.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {Route, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-master-chat',
+  templateUrl: './master-chat.component.html',
+  styleUrls: ['./master-chat.component.scss']
+})
+export class MasterChatComponent {
+  constructor(private route:Router) {
+    //this.route.navigate(["chats/string"]);
+  }
+  OpenChat(name:string){
+    this.route.navigate([`chats/${name}`]);
+  }
+}
---
+++ src/app/shared/context-menu/context-menu.component.ts
@@ -0,0 +1,27 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+export class ContextMenuModel {
+    menuText?: string
+    menuEvent?: string
+}
+
+@Component({
+  selector: 'app-context-menu',
+  templateUrl: './context-menu.component.html',
+  styleUrls: ['./context-menu.component.scss']
+})
+export class ContextMenuComponent {
+  @Input()
+  contextMenuItems?: Array<ContextMenuModel>;
+
+  @Output()
+  onContextMenuItemClick: EventEmitter<any> = new EventEmitter<any>();
+
+  onContextMenuClick(event:Event, data?:ContextMenuModel): any {
+    if(data)
+      this.onContextMenuItemClick.emit({
+        event,
+        data,
+      });
+  }
+}
